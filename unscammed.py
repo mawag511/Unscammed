39f16c6fe0bcc0d3fa6e190bff21fc16949f23c5
Maria <ms.marusenka@list.ru>
2023-12-15 23:53:54 +0200

    Completed main app functionalities

--- unscammed.py
+++ unscammed.py
@@ -1,105 +1,97 @@
-from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtGui import QCursor, QIcon
+from PyQt5.QtGui import  QIcon, QCursor, QMovie
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt, QTimer, QPoint
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.uic import loadUi
 import sys, time
 
 from scam_locator import message_check
 from resources import logo_resource
 
-app = QtWidgets.QApplication(sys.argv)
-widgets = QStackedWidget()
-
-
+''' Splash Screen'''
 class SplashScreen(QDialog):
     def __init__(self):
         super(SplashScreen, self).__init__()
         loadUi("./GUIs/SplashScreenGUI.ui", self)
+        self.setWindowFlags(Qt.FramelessWindowHint)  
         self.setFixedHeight(495)
         self.setFixedWidth(760)
+        self.movie = QMovie("./resources/assets/splash_screen.gif")
+        self.movie.setScaledSize(self.size())
         self.timer = QTimer()
+        self.label.setMovie(self.movie)
         self.timer.timeout.connect(self.loading)
-        self.timer.start(60)
-       #widgets.setWindowFlags(Qt.FramelessWindowHint)   <-- to fix
+        self.timer.start(4000)
+        self.movie.start()
+        
    
     def loading(self):
         self.timer.stop()
-        time.sleep(3)
-        main_menu = MainMenu()
-        widgets.addWidget(main_menu)
-        widgets.setCurrentIndex(widgets.currentIndex()+1)
-        widgets.setWindowTitle("UNSCAMMED")
-        #widgets.setWindowIcon(QIcon("icon.png"))   <-- to fix
-        widgets.setFixedHeight(695)
-        widgets.setFixedWidth(880) 
+        time.sleep(1)
+        window = Main()
+        window.show()
+        self.close()
+      
 
-
-class MainMenu(QDialog):
+''' Main Application'''
+class Main(QMainWindow):
+    ## initialization of all app and first page
     def __init__(self):
-        super(MainMenu, self).__init__()
-        loadUi("./GUIs/MainMenuGUI.ui", self)
-        
-        self.SpamDetectionBTN.clicked.connect(self.go_to_scam_locator)
+        super(Main, self).__init__()
+        loadUi("./GUIs/ApplicationGUI.ui", self)
+        self.setWindowTitle("UNSCAMMED")
+        self.setWindowIcon(QIcon("./resources/assets/icon.png")) 
+        self.setFixedHeight(695)
+        self.setFixedWidth(880) 
+        self.stackedWidget.setCurrentWidget(self.main_menu)
 
-    def go_to_scam_locator(self):
-        scam_locator = ScamLocator()
-        widgets.addWidget(scam_locator)
-        widgets.setCurrentIndex(widgets.currentIndex()+1)
+        ## buttons
+        self.SpamDetectionBTN.clicked.connect(self.open_clear)
+        self.SubmitBTN.clicked.connect(self.check_message)
+        self.GoBackBTN.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.main_menu))
+        self.GoBackBTN_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.SL_submission))
 
+    def open_clear(self):
+        self.stackedWidget.setCurrentWidget(self.SL_submission)
+        self.WarningLabel.setText("")
 
-class ScamLocator(QDialog):
-    def __init__(self):
-        super(ScamLocator, self).__init__()
-        loadUi("./GUIs/ScamLocatorSubmissionGUI.ui", self)
-        self.SubmitBTN.clicked.connect(self.message_check)
-        self.GoBackBTN.clicked.connect(self.go_back_to_main_menu)
-
-    def message_check(self):
-        global input
-        global results
+    ## check user input against ML algorithms
+    def check_message(self):
         input = self.UserInput.toPlainText()
 
         if input != "":
             self.WarningLabel.setText("")
             results = message_check(str(input))
-            self.go_to_scam_locator_res()
+            self.stackedWidget.setCurrentWidget(self.SL_results)
+            self.Results.setText(results)
+            self.CheckedMessage.setText(input)
+            self.UserInput.setText("")
         else:
             self.WarningLabel.setText("Please insert a message to verify!")
-   
-    def go_to_scam_locator_res(self):
-        scam_locator_res = ScamLocatorResults()        
-        widgets.addWidget(scam_locator_res)
-        widgets.setCurrentIndex(widgets.currentIndex()+1)
-        scam_locator_res.Results.setText(results)
-        scam_locator_res.CheckedMessage.setText(input)
 
-    def go_back_to_main_menu(self):
-        main_menu = MainMenu()
-        widgets.addWidget(main_menu)
-        self.close()
-        widgets.setCurrentIndex(widgets.currentIndex()+1)
-      
+    ## drag frame
+    def mousePressEvent(self, event):
+        if event.button()==Qt.LeftButton:
+            self.m_flag=True
+            self.m_Position=event.globalPos()-self.pos() #Get the position of the mouse relative to the window
+            event.accept()
+            self.setCursor(QCursor(Qt.OpenHandCursor)) #Change the mouse icon
+            
+    def mouseMoveEvent(self, QMouseEvent):
+        if Qt.LeftButton and self.m_flag:  
+            self.move(QMouseEvent.globalPos()-self.m_Position)#Change window position
+            QMouseEvent.accept()
+                
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_flag=False
+        self.setCursor(QCursor(Qt.ArrowCursor))
 
-class ScamLocatorResults(QDialog):
-    def __init__(self):
-        super(ScamLocatorResults, self).__init__()
-        loadUi("./GUIs/ScamLocatorResultsGUI.ui", self)
-        self.GoBackBTN.clicked.connect(self.go_back_to_scam_locator)
 
-    def go_back_to_scam_locator(self):
-        scam_locator = ScamLocator()        
-        widgets.addWidget(scam_locator)
-        widgets.setCurrentIndex(widgets.currentIndex()+1)
- 
+## execute app
 def main():
+    app = QApplication(sys.argv)
     splash_screen = SplashScreen()
-    widgets.addWidget(splash_screen)
-
-    widgets.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, False)
-    widgets.show()
+    splash_screen.show()
     sys.exit(app.exec_() )
-
 
 if __name__ == "__main__":
    main()
